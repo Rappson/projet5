29e0243daff1fb43c45c7f391d1cdfbe7e0f850c
Rappson <69462172+Rappson@users.noreply.github.com>
2021-05-09 16:56:17 +0200

    2eme essai du formulaire

--- src/form.js
+++ src/form.js
@@ -1,36 +1,23 @@
 const inputName = document.querySelector('#inputName');
-const inputfirstName = document.querySelector('#firstName');
+const inputFirstName = document.querySelector('#firstName');
 const inputMail = document.querySelector('#inputMail');
 const inputTel = document.querySelector('#inputTel');
 const inputAdress = document.querySelector('#inputAdress');
+const inputCity = document.querySelector('#inputCity');
+const inputZip = document.querySelector('#inputZip');
 
 // TEMPORAIRE
 const submitFormBtn = document.querySelector('.btn-primary')
+
 submitFormBtn.addEventListener('click', function(e){
      e.preventDefault()
+     let contact = {}
+     contact.name = inputName.textContent;
+     contact.familyName = inputFirstName.textContent;
+     // contact.adress = inputAdress.textContent;
+     contact.city = inputCity.textContent + "," + inputZip.textContent;
+     console.log(contact);
 })
 
-function formulaire (){
-     let userName = inputName.addEventListener('input', function(e){
-          inputName.value
-      });
-       let userFirstName = null;
-       let userAdress = null;
-       let userCity = null;
-       let userEmail = null;
-      
-      
-      
-      
-       class contact {
-           constructor(firstName, name, adress, city, email){
-                this.firstName = firstName;
-                this.name = name;
-                this.adress = adress;
-                this.city = city;
-                this.email = email;
-           }
-      }
-      let newContact = new contact (userName, userFirstName, userAdress, userCity, userEmail)
-      console.log(newContact);
-}
+
+
---
+++ src/test.js
@@ -0,0 +1,24 @@
+// function formulaire (){
+//      let userName = inputName.addEventListener('input', function(e){
+//           inputName.value
+//       });
+//        let userFirstName = null;
+//        let userAdress = null;
+//        let userCity = null;
+//        let userEmail = null;
+      
+      
+      
+      
+//        class contact {
+//            constructor(firstName, name, adress, city, email){
+//                 this.firstName = firstName;
+//                 this.name = name;
+//                 this.adress = adress;
+//                 this.city = city;
+//                 this.email = email;
+//            }
+//       }
+//       let newContact = new contact (userName, userFirstName, userAdress, userCity, userEmail)
+//       console.log(newContact);
+// }
