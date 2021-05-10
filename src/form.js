0164b81b71c9a82b266bbac5708b0c5c10d45976
Rappson <69462172+Rappson@users.noreply.github.com>
2021-05-10 11:51:02 +0200

    test regex

--- src/form.js
+++ src/form.js
@@ -2,21 +2,31 @@ const inputName = document.querySelector('#inputName');
 const inputFirstName = document.querySelector('#firstName');
 const inputMail = document.querySelector('#inputMail');
 const inputTel = document.querySelector('#inputTel');
-const inputAdress = document.querySelector('#inputAdress');
+const inputAddress = document.querySelector('#inputAddress');
 const inputCity = document.querySelector('#inputCity');
 const inputZip = document.querySelector('#inputZip');
 
-// TEMPORAIRE
+// commencement de la function de recuperation de données
 const submitFormBtn = document.querySelector('.btn-primary')
-
-submitFormBtn.addEventListener('click', function(e){
+submitFormBtn.addEventListener('click', function (e) {
      e.preventDefault()
+     
+     var init = {
+          method: 'POST',
+          header: {
+               'accept':'application/json',
+               'Content-Type': 'application/json'
+          }
+     };
+     // recuperation des données utilisateur
      let contact = {}
-     contact.name = inputName.textContent;
-     contact.familyName = inputFirstName.textContent;
-     // contact.adress = inputAdress.textContent;
-     contact.city = inputCity.textContent + "," + inputZip.textContent;
+     contact.name = inputName.value;
+     contact.familyName = inputFirstName.value;
+     contact.address = inputAddress.value;
+     contact.city = inputCity.value + "," + inputZip.value;
+
      console.log(contact);
+
 })
 
 
