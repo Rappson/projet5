1270c1d6eba107cbbf108029ff9137b941eed415
Rappson <69462172+Rappson@users.noreply.github.com>
2021-05-16 13:41:02 +0200

    mise en place de la page de remerciement

--- src/form.js
+++ src/form.js
@@ -4,7 +4,7 @@
 // console.log(maVariable);
 
 
-function test (form, e) {
+function form(form, e) {
      e.preventDefault()
      let data = {};
 
@@ -17,21 +17,36 @@ function test (form, e) {
      contact.email = form.email.value;
 
      let products = [];
-     for ( let i = 0; i < localStorage.length; i++){
+     for (let i = 0; i < localStorage.length; i++) {
           products.push(localStorage.key(i))
      }
 
-    order = {
-         contact: contact,
-         products: products // todo à créer
-    }
-    var init = {
-         method: 'post',
-         body: JSON.stringify(order)
-    }
-    // post data fetch 
-     fetch(urlBase + "cameras/"+ "order", init)
-     
-     console.log(init);
+     order = {
+          contact: contact,
+          products: products // todo à créer
+     }
+     var init = {
+          headers: {
+               'Accept': 'application/json',
+               'Content-Type': 'application/json'
+          },
+          method: 'POST',
+          body: JSON.stringify(order)
+     }
+     // post data fetch 
+     fetch(urlBase + "cameras/" + "order", init)
+     .then(Response => Response.json())
+     .then(data => {
+          // target de la div de remerciement
+          let congrat = document.querySelector('#order-congrat')
+          if (congrat.classList.contains ("disabled")){
+               congrat.classList.replace('disabled', 'congrat-active')
+               let orderIdCongrat = document.createElement('p')
+               congrat.appendChild(orderIdCongrat)
+               orderIdCongrat.innerHTML = 'Numéro de commande : ' + data.orderId
+          }
+          console.log(data);
+
+     })
 }
 
