408731443bf0e15c65401c5673f4dc4aeb7f1fba
Rappson <69462172+Rappson@users.noreply.github.com>
2021-05-16 20:38:34 +0200

    mise en place du résumé de commande

--- src/form.js
+++ src/form.js
@@ -35,29 +35,58 @@ function form(form, e) {
      }
      // post data fetch 
      fetch(urlBase + "cameras/" + "order", init)
-     .then(Response => Response.json())
-     .then(data => {
-          // target de la div de remerciement
-          let congrat = document.querySelector('#order-congrat')
-          if (congrat.classList.contains ("disabled")){
-               congrat.classList.replace('disabled', 'congrat-active')
-               let orderIdCongrat = document.createElement('a')
-               orderIdCongrat.classList.add('congrat-content')
-               orderIdCongrat.setAttribute('href', `targetOrder.html?orderId= ${data.orderId}`)
-               congrat.appendChild(orderIdCongrat)
-               orderIdCongrat.innerHTML = 'Numéro de commande : ' + data.orderId
-          }
-          console.log(data);
+          .then(Response => Response.json())
+          .then(data => {
+               // target de la div de remerciement
+               let congrat = document.querySelector('#order-congrat')
+               if (congrat.classList.contains("disabled")) {
+                    congrat.classList.replace('disabled', 'congrat-active')
+                    let orderIdCongrat = document.createElement('a')
+                    orderIdCongrat.classList.add('congrat-content')
+                    orderIdCongrat.setAttribute('href', `targetOrder.html?orderId=${data.orderId}`)
+                    congrat.appendChild(orderIdCongrat)
+                    orderIdCongrat.innerHTML = 'Numéro de commande : ' + data.orderId
+               }
+               localStorage.clear()
+               localStorage.setItem(data.orderId, JSON.stringify(data))
 
-     })
+          })
 }
 
-     var searchId = new URLSearchParams(window.location.search)
-    if (searchId.has('orderId')){
+
+// afffichage de la commande
+
+let orderContent = document.querySelector('.order-content')
+
+
+var searchId = new URLSearchParams(window.location.search)
+if (searchId.has('orderId')) {
      var targetOrderId = searchId.get('orderId')
-     fetch(urlBase + 'cameras/'+ 'order/' + targetOrderId)
-     .then(Response => Response.json())
-     .then(data =>{
-          console.log(data);
-     })
-    }
+
+     // mise en place du texte d'acceuil
+     let jumbotronId = document.createElement('div')
+     orderContent.appendChild(jumbotronId)
+     jumbotronId.classList.add('jumbotron')
+     jumbotronId.innerHTML = `Résumé de la commande n°${targetOrderId}`
+
+     // recuperation des données du ls
+     var lsKeyOrder = JSON.parse(localStorage.getItem(targetOrderId))
+     var targetProducts = lsKeyOrder.products
+     console.log(lsKeyOrder);
+
+     // creation du container des produits
+     let productContainer = document.createElement('div')
+     orderContent.appendChild(productContainer)
+     productContainer.classList.add('row')
+
+     for (let product of targetProducts) {
+          let divProducts = document.createElement('div')
+          productContainer.appendChild(divProducts)
+          divProducts.classList.add('row')
+          divProducts.innerHTML = `<a href='product.html?${product._id}'> <img src='${product.imageUrl}' alt='image du produit' width='10%'></img> </a>
+           <p class='produt_order_name'>${product.name}</p>
+            &nbsp;&nbsp;&nbsp; 
+            <p>${product.price} $</p>`
+
+     }
+}
