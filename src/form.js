16e5283751e0779e9f90eb6f473ba2f9518c6ac9
Grolier Axel <axel.grolier@gmail.com>
2021-05-30 15:46:47 +0200

    mise en propre supplémentaire

--- src/form.js
+++ src/form.js
@@ -26,36 +26,41 @@ function form(form, e) {
           method: 'POST',
           body: JSON.stringify(order)
      }
-     
+
      // post data fetch 
      fetch(urlBase + "cameras/" + "order", init)
           .then(Response => Response.json())
           .then(data => {
                // target de la div de remerciement
                let congrat = document.querySelector('#order-congrat')
-               if (congrat.classList.contains("disabled")) {
-                    congrat.classList.replace('disabled', 'congrat-active')
 
-                    // mise en place du total dans le menu de remerciement
-                    let totalOrder = document.createElement('p')
-                    let sumOrder = 0;
-                    for (i = 0; i < localStorage.length; i++) {
-                         let lstarget = localStorage.key(i)
-                         let targetJson = JSON.parse(localStorage.getItem(lstarget))
-                         let elementTotal = targetJson.price * targetJson.quantity
-                         sumOrder = sumOrder + elementTotal
+               function congratulationMessage() {
+                    if (congrat.classList.contains("disabled")) {
+                         congrat.classList.replace('disabled', 'congrat-active')
+
+                         // mise en place du total dans le menu de remerciement
+                         let totalOrder = document.createElement('p')
+                         let sumOrder = 0;
+                         for (i = 0; i < localStorage.length; i++) {
+                              let lstarget = localStorage.key(i)
+                              let targetJson = JSON.parse(localStorage.getItem(lstarget))
+                              let elementTotal = targetJson.price * targetJson.quantity
+                              sumOrder = sumOrder + elementTotal
+                         }
+                         congrat.appendChild(totalOrder)
+                         totalOrder.innerHTML = `Total: ${sumOrder} $`
+                         totalOrder.classList.add('congrat-content', 'h3')
+
+                         let orderIdCongrat = document.createElement('a')
+                         orderIdCongrat.classList.add('congrat-content')
+                         orderIdCongrat.setAttribute('href', `#`)
+                         congrat.appendChild(orderIdCongrat)
+                         orderIdCongrat.innerHTML = 'Numéro de commande : ' + data.orderId
+
                     }
-                    congrat.appendChild(totalOrder)
-                    totalOrder.innerHTML = `Total: ${sumOrder} $`
-                    totalOrder.classList.add('congrat-content', 'h3')
-
-                    let orderIdCongrat = document.createElement('a')
-                    orderIdCongrat.classList.add('congrat-content')
-                    orderIdCongrat.setAttribute('href', `#`)
-                    congrat.appendChild(orderIdCongrat)
-                    orderIdCongrat.innerHTML = 'Numéro de commande : ' + data.orderId
-
                }
+               congratulationMessage();
+
                localStorage.clear()
           })
 }
