4182d8cb6e9a195e242cda190bb148a80966a277
Axel Grolier <69462172+Rappson@users.noreply.github.com>
2021-06-27 22:02:09 +0200

    mise à jour product

--- src/product.js
+++ src/product.js
@@ -1,25 +1,25 @@
 function productFocused() {
     if (searchParams.has('id')) {
         console.log(getProductId);
-        
+
         fetch(urlBase + 'cameras/' + getProductId)
             .then(response => response.json())
             .then(data => {
                 console.log(data);
                 let containerProduct = document.querySelector('.container-product')
-                
+
                 if (data.imageUrl == undefined ||
                     data.name == undefined ||
-                    data.price == undefined){
-                        let errorProduct = document.createElement('p');
-                        let linkToMain = document.createElement('a');
-                        errorProduct.innerHTML = "une erreur est survenue ! Merci de retourner à l'accueil";
-                        linkToMain.innerHTML = "Cliquez ici pour retourner à l'accueil";
-                        linkToMain.setAttribute('href', 'index.html');
+                    data.price == undefined) {
+                    let errorProduct = document.createElement('p');
+                    let linkToMain = document.createElement('a');
+                    errorProduct.innerHTML = "une erreur est survenue ! Merci de retourner à l'accueil";
+                    linkToMain.innerHTML = "Cliquez ici pour retourner à l'accueil";
+                    linkToMain.setAttribute('href', 'index.html');
 
-                        containerProduct.appendChild(errorProduct)
-                        containerProduct.appendChild(linkToMain)
-                }else{
+                    containerProduct.appendChild(errorProduct)
+                    containerProduct.appendChild(linkToMain)
+                } else {
                     containerProduct.innerHTML = `
                 <img src="${data.imageUrl}" alt="" id="product_img" width="70%">
                 <!-- infos -->
@@ -29,41 +29,49 @@ function productFocused() {
                 <!-- lenses -->
                 <div class="list-group lense lenses-container">
         
-                </div>
-                <button type="submit" class="btn btn-outline-primary btn-rounded add-to-panier">Ajouter au panier</button>`
-                
-
-                // incorporation des lentilles
-                for (var i = 0; i < data.lenses.length; i++) {
-                    // création des liens de personnalisation
-                    const productLense = document.querySelector('div.lense');
-                    let lenseBtn = document.createElement('a')
-                    productLense.appendChild(lenseBtn)
-                    lenseBtn.setAttribute('class', 'list-group-item list-group-item-action');
-                    lenseBtn.setAttribute('href', '#')
-                    lenseBtn.innerHTML = data.lenses[i];
+                </div>`;
+                    
 
 
-                    // mettre en active la personalisation selectionné
-                    lenseBtn.addEventListener('click', function (e) {
-                        e.preventDefault()
-                        if (lenseBtn.classList.contains('active')) {
-                            lenseBtn.classList.remove('active', 'active-user-choose')
-                            let positionTab = tabLense.indexOf(lenseBtn.textContent);
-                            tabLense.splice(positionTab, 1)
-                        } else {
-                            lenseBtn.classList.add('active', 'active-user-choose')
-                            tabLense.push(lenseBtn.textContent)
+                    // incorporation des lentilles
+                    for (var i = 0; i < data.lenses.length; i++) {
+                        // création des liens de personnalisation
+                        const productLense = document.querySelector('div.lense');
+                        let lenseBtn = document.createElement('a')
+                        productLense.appendChild(lenseBtn)
+                        lenseBtn.setAttribute('class', 'list-group-item list-group-item-action');
+                        lenseBtn.setAttribute('href', '#')
+                        lenseBtn.innerHTML = data.lenses[i];
 
-                        }
-                    })
-                }
+
+                        // mettre en active la personalisation selectionné
+                        lenseBtn.addEventListener('click', function (e) {
+                            e.preventDefault()
+                            if (lenseBtn.classList.contains('active')) {
+                                lenseBtn.classList.remove('active', 'active-user-choose')
+                                let positionTab = tabLense.indexOf(lenseBtn.textContent);
+                                tabLense.splice(positionTab, 1)
+                            } else {
+                                lenseBtn.classList.add('active', 'active-user-choose')
+                                tabLense.push(lenseBtn.textContent)
+
+                            }
+                        })
+                    }
                 }
             })
     }
 }
 
 function putProductOnCart() {
+    let containerBtn = document.querySelector('.container-btn-submit');
+    let btnAddToCart = document.createElement('button');
+    btnAddToCart.setAttribute('type', 'submit');
+    btnAddToCart.setAttribute('class', 'btn btn-outline-primary btn-rounded add-to-panier');
+    btnAddToCart.innerHTML = 'Ajouter au panier'
+    containerBtn.appendChild(btnAddToCart);
+
+
     for (i = 0; i < addToPanierSelector.length; i++) {
         addToPanierSelector[i].addEventListener('click', function () {
 
